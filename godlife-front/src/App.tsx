97e87153079f0a1804fabf435bfd4f79d46bdbb0
Jinwoo Park <skin_lotion@naver.com>
2023-09-14 08:59:20 +0900

    HTML,CSS_1

--- godlife-front/src/App.tsx
+++ godlife-front/src/App.tsx
@@ -1,9 +1,13 @@
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import './App.css';
 import React from 'react';
 import './App.css';
+import Authentication from './views/Authentication';
+import { AUTH_PATH } from './views/constant';
 
 function App() {
   return (
-    <></>
+    <Authentication/>
   );
 }
 
--- godlife-front/src/views/Authentication/index.tsx
+++ godlife-front/src/views/Authentication/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import './style.css';
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+//                      component : 메일 링크 전송 알림 페이지                //
+export default function Authentication() {
+    //                      render : 메일 링크 전송 알림 랜더링                    //
+    return(
+        <div id='auth-wrapper'>
+            <div className='auth-container'>
+                <div className='auth-card'>
+                    <div className='auth-card-top-container'>
+                        <div className='logo-icon-box'>
+                            <div className='logo-icon'></div>
+                        </div>
+                        <div className='auth-card-top-text'>{'받은 메일함을 확인하여 로그인 하세요'}</div>
+                    </div>
+                    <div className='auth-card-middle-container'>
+                        <div className='auth-card-middle-icon-box'>
+                            <div className='email-logo-icon'></div>
+                        </div>
+                        <div className='auth-card-middle-text'>{'설정을 완료하고 로그인하려면 다음 주소로 보낸 이메일의 \n확인 링크를 클릭하세요.'}</div>
+                        <div className='auth-card-middle-send-email'>{'skin_lotion@naver.com'}</div>
+                    </div>
+                    <div className='auth-card-bottom-container'>
+                        <div className='auth-card-bottom-return-link-text'>{'확인 이메일 다시 받기'}</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
