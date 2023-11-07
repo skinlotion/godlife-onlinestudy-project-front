0090ee6d7b72aa1da2d31378d783ce74de3c08cf
khkim <stakkh4@gmail.com>
2023-11-07 13:46:20 +0900

    스터디  시작 시간 렌더링, 시간 설정 클릭 이벤트,시작시간 박스  클릭 이벤트

--- godlife-front/src/Modals/StudyDateModal/index.tsx
+++ godlife-front/src/Modals/StudyDateModal/index.tsx
@@ -1,9 +1,13 @@
 import './style.css';
 import  dayjs from 'dayjs';
-
-import React, { useState } from 'react';
+import ModalSideMenu from 'components/ModalSideMenu';
+import React, { useState,useRef,  useEffect, ChangeEvent  } from 'react';
+import Scrollbars from 'react-custom-scrollbars-2';
 
 export default function StudyDate(){
+
+    // state : 스터디 시간 미설정에 대한 오류 여부  상태 //
+    const [timeErrer, setTimeError] = useState<boolean>(false);
 
     const [isClicked, setIsClicked] = useState(false);
 
@@ -11,92 +15,107 @@ export default function StudyDate(){
         setIsClicked(!isClicked);
     };
   
-  // component : 시간 선택 콤보 박스 //
-  const HourComboBox = () => {
+    // component :   스터디 경고 메시지 컴포넌트  //
+    const StudyAlertCard = () => {
 
-    return(
-        <div className='study-box-wrapper'>
-            <div className="study-hour-select">
-                {isClicked && (
-                    <div>
-                        <select className='select-size'>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                        </select>
-                    </div>
-                )}
-            </div>
+        // state : 스터디 일정 에러 여부 //
+        const [dateError, setDateError] = useState<boolean>(false);
 
-            { !isClicked &&
-            <div className='study-hour-icon-box' onClick={handleClick}>
-                <div className="study-hour-icon" ></div>
+        return(
+            <div className='study-date-contents-box'>
+                <div className='study-date-contents-icon'></div>
+            {   
+                <div className="study-date-contents-message">{'캘린더에서 다음 스터디 일정을 선택해주세요!'}</div>
+                // <div className='study-date-contents-error-message'>{'캘린더에서 다음 스터디 일정을 선택해주세요!'}</div>
+            }   
             </div>
+        )
+    }
+
+    // component : 시작 시간(hour) 선택 콤보 박스 컴포넌트   //
+    const StartHourDropDownBox = () =>{
+
+        //          state: 박스 상태          //
+        const [isOpen, setIsOpen] = useState(false);
+        //          state: 박스 선택 상태          //
+        const [selectedItem, setSelectedItem] = useState<string | null>(null);
+        //          state: 박스 드롭다운 ref 상태          //
+        const dropdownRef = useRef<HTMLDivElement | null>(null);
+
+
+        //          event handler: 시작시간 박스  열기 클릭 이벤트 처리          //
+        const toggleDropdownClickHandler = () => {
+            setIsOpen(!isOpen);
+        };
+
+        //          function: 박스 항목 선택 호출 함수          //
+        const selectItem = (item: string) => {
+            setSelectedItem(item);
+            setIsOpen(false);
+        };
+
+
+        //          effect: 박스가 아닌 다른 곳을 클릭하면 박스가 사라지게 하기          //
+        useEffect(() => {
+            function handleClickOutside(event: MouseEvent) {
+                if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                    setIsOpen(false);
+                }
             }
-        </div>
-    )
-  }
-  // component :   스터디 경고 메시지 컴포넌트  //
-  const StudyAlertCard = () => {
 
-    // state : 스터디 일정 에러 여부 //
+            document.addEventListener('click', handleClickOutside);
+            return () => {
+                document.removeEventListener('click', handleClickOutside);
+            };
+        }, []);
 
-    return(
-        <div className='study-date-contents-box'>
-            <div className='study-date-contents-icon'></div>
-        {   
-            <div className="study-date-contents-message">{'캘린더에서 다음 스터디 일정을 선택해주세요!'}</div>
-            // <div className='study-date-contents-error-message'>{'캘린더에서 다음 스터디 일정을 선택해주세요!'}</div>
-        }   
-        </div>
-    )
-  }
+        // description :   스터디 시작  시간(hour) 선택 항목 생성           //
+        const items = [];
+        for (let i = 0; i <= 11; i++) {
+            items.push(`${i}`);
+        }
 
-  // component : 캘린더 설정 컴포넌트 //
-  const CalendarCard = () => {
-
-    // state :              //
-
-    return(
-        <div className='study-date-set-box'>
-            <div className="study-date-schedule">{`스터디 일정 : 00 00 00`}</div>
-            <div className="study-date-hour-box">
-                <div className="study-date-hour-title">{'스터디 시간: '}</div>
-                <HourComboBox/>
-                <div className='division'>{':'}</div>
-                <HourComboBox/>
-                <div className="between">{'~'}</div>
-                <HourComboBox/>
-                <div className='division'>{':'}</div>
-                <HourComboBox/>
+        //          event handler: 스터디 시간 설정 클릭 이벤트 처리          //
+        const startHourChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+            if(!event.target.value ){ 
+                setTimeError(true);
+                return;
+            }
+            const startHour  = event.target.value;
+            setSelectedItem(startHour);
+        };
+        
+        //          render: DropDown 스터디 시작 시간(hour) 설정 렌더링          //
+        return (
+            <div ref={dropdownRef} className='dropdown-study-people-set-box'>
+                <div className='study-hour-select'onChange={startHourChangeHandler} >{selectedItem}</div>
+                <div className='down-icon-box'  onClick={toggleDropdownClickHandler}>
+                    <div className='down-icon'></div>
+                </div>
+                {isOpen && (
+                    <div className='dropdown-study-people-set-list'>
+                        <Scrollbars 
+                            renderTrackVertical={(props) => <div {...props} className='track-people-set-vertical' />} 
+                            renderThumbVertical={(props) => <div {...props} className='thumb-people-set-vertical' />}>
+                            {items.map((item) => (
+                                <div className='dropdown-study-people-set-list-index' key={item} onClick={() => selectItem(item)}>
+                                    {item}
+                                </div>
+                            ))}
+                        </Scrollbars>
+                    </div>
+                )}  
             </div>
-        </div>
-    )
-  }
+        );
+    }
+
 
 
   // render : 다음 스터디 일저 렌더링 //
   return (
     <div id='notice-wrapper'>
        <div className="notice-card">
-           <div className='menu-sidebar'>
-            {
-                <div className="menu-room">방 설정</div>
-                // <div className='menu-room-default'></div>
-            }
-            {
-                // <div className="menu-member">멤버 관리</div>
-                <div className="menu-member-default">멤버 관리</div>   
-            }
-            {
-                // <div className="menu-study">{'다음 스터디 설정'}</div>
-                <div className='menu-study-default'>{'다음 스터디 설정'}</div>
-            }
-            {
-                // <div className="menu-material">자료관리</div>
-                <div className="menu-material-default">자료관리</div>
-            }
-            </div>
+           <ModalSideMenu/>
             <div className="notice-button-box">
                 <button type="button" className="btn btn-primary" disabled data-bs-toggle="button">X</button>        
             </div>
@@ -110,7 +129,7 @@ export default function StudyDate(){
                         <div className='study-date-box'>
                             {
                                 // <StudyAlertCard/> 
-                                <CalendarCard/>
+                              
                             }
 
                         </div>
